18eca56322095423591524b5106625e3c5918430
lgants <lgants@gmail.com>
2016-08-21 15:15:42 -0400

    basic nested forms

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
   has_many :addresses
-
+  #provides ability to call addresses_attributes=
+  accepts_nested_attributes_for :addresses
+  #When we called new_person.save it created both the Person object and the two Address objects we passed
 end
